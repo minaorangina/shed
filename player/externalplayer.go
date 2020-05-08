e8f9013b88d69855aea5c97f79851a2474281adb
Mina <wilhelmina.jane@gmail.com>
2020-05-08 17:09:26 +0100

    creates ExternalPlayer type

---
+++ player/externalplayer.go
@@ -0,0 +1,26 @@
+package player
+
+import "os"
+
+type conn struct {
+	In  *os.File
+	Out *os.File
+}
+
+// ExternalPlayer represents a player in the outside world
+// An ExternalPlayer maps onto a Player in the game
+type ExternalPlayer struct {
+	ID   string
+	Name string
+	Conn *conn // tcp or command line
+}
+
+// NewExternalPlayer constructs an ExternalPlayer
+func NewExternalPlayer(id, name string) ExternalPlayer {
+	conn := &conn{os.Stdin, os.Stdout}
+	return ExternalPlayer{
+		ID:   id,
+		Name: name,
+		Conn: conn,
+	}
+}
