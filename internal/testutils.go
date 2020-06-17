edc0db0e9839c6dc383a5410895832dbeedad8fc
Mina <wilhelmina.jane@gmail.com>
2020-06-17 15:17:26 +0100

    adds testing helpers

--- internal/testutils.go
+++ internal/testutils.go
@@ -6,17 +6,17 @@ import (
 )
 
 // FailureMessage returns a failure message for a failed test
-func FailureMessage(t *testing.T, expected, actual interface{}) {
-	expectedString := TypeToString(expected)
-	actualString := TypeToString(actual)
-	t.Errorf("\nExpected: %s\nActual: %s", expectedString, actualString)
+func FailureMessage(t *testing.T, want, got interface{}) {
+	expectedString := TypeToString(want)
+	actualString := TypeToString(got)
+	t.Errorf("\nWant: %s\nGot: %s", expectedString, actualString)
 }
 
 // TableFailureMessage returns a failure message for a failed test, including the name of the test
-func TableFailureMessage(t *testing.T, testName, expected, actual interface{}) {
-	expectedString := TypeToString(expected)
-	actualString := TypeToString(actual)
-	t.Errorf("%s\nExpected: %s\nActual: %s", testName, expectedString, actualString)
+func TableFailureMessage(t *testing.T, testName, want, got interface{}) {
+	expectedString := TypeToString(want)
+	actualString := TypeToString(got)
+	t.Errorf("%s\nWant: %s\nGot: %s", testName, expectedString, actualString)
 }
 
 // TypeToString returns the string representation of a non-string type
@@ -24,9 +24,16 @@ func TypeToString(obj interface{}) string {
 	return fmt.Sprintf("%+v", obj)
 }
 
-// AssertsNoError checks for the non-existence of an error
+// AssertNoError checks for the non-existence of an error
 func AssertNoError(t *testing.T, err error) {
 	if err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
+	}
+}
+
+// AssertEqual checks that the values are equal
+func AssertEqual(t *testing.T, want, got interface{}) {
+	if want != got {
+		FailureMessage(t, want, got)
 	}
 }
