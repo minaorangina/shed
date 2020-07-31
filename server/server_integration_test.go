509e42f01590b03cc5b627719d2865229386ba3a
Mina <wilhelmina.jane@gmail.com>
2020-07-31 16:30:33 +0100

    move all tests into one file

--- server/server_integration_test.go
+++ server/server_integration_test.go
@@ -4,53 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
-
-func TestServerPing(t *testing.T) {
-	response := httptest.NewRecorder()
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
-
-	server := NewServer(nil)
-	server.ServeHTTP(response, request)
-
-	assertStatus(t, response.Code, http.StatusOK)
-}
-func TestPOSTNewGame(t *testing.T) {
-	t.Run("succeeds and returns expected data", func(t *testing.T) {
-		data, _ := json.Marshal(NewGameReq{"Elton"})
-
-		response := httptest.NewRecorder()
-		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer(data))
-
-		server := NewServer(nil)
-		server.ServeHTTP(response, request)
-
-		assertStatus(t, response.Code, http.StatusCreated)
-		assertNewGameResponse(t, response.Body, "Elton")
-	})
-
-	t.Run("returns 400 if the player's name is missing", func(t *testing.T) {
-		response := httptest.NewRecorder()
-		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer([]byte{}))
-		server := NewServer(nil)
-		server.ServeHTTP(response, request)
-
-		assertStatus(t, response.Code, http.StatusBadRequest)
-	})
-
-	t.Run("Does not match on GET /new", func(t *testing.T) {
-		response := httptest.NewRecorder()
-		request, _ := http.NewRequest(http.MethodGet, "/new", nil)
-
-		server := NewServer(nil)
-		server.ServeHTTP(response, request)
-
-		assertStatus(t, response.Code, http.StatusNotFound)
-	})
-}
 
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +13,50 @@ import (
 	"github.com/minaorangina/shed/players"
 )
 
-func TestServer(t *testing.T) {
+func TestServerPing(t *testing.T) {
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	server := NewServer(nil)
+	server.ServeHTTP(response, request)
+
+	assertStatus(t, response.Code, http.StatusOK)
+}
+func TestServerPOSTNewGame(t *testing.T) {
+	t.Run("succeeds and returns expected data", func(t *testing.T) {
+		data, _ := json.Marshal(NewGameReq{"Elton"})
+
+		response := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer(data))
+
+		server := NewServer(nil)
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusCreated)
+		assertNewGameResponse(t, response.Body, "Elton")
+	})
+
+	t.Run("returns 400 if the player's name is missing", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer([]byte{}))
+		server := NewServer(nil)
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusBadRequest)
+	})
+
+	t.Run("Does not match on GET /new", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodGet, "/new", nil)
+
+		server := NewServer(nil)
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+}
+
+func TestServerGETGame(t *testing.T) {
 	t.Run("return an existing game", func(t *testing.T) {
 		testID := "12u34"
 		server := newServerWithGame(newTestGame(t, testID, nil, nil))
